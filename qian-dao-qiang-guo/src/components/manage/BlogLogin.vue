99e5318358ead8a4fea53171edb15d5a16e67673
Conan <lipeiguan@gmail.com>
2019-05-24 19:18:48 +0800

    update

--- qian-dao-qiang-guo/src/components/manage/BlogLogin.vue
+++ qian-dao-qiang-guo/src/components/manage/BlogLogin.vue
@@ -1,25 +1,39 @@
 <template>
-  <div>
-    <blog-header></blog-header>
-    <hr>
-    <div>
-      用户名:
-      <input type="text" v-model="User.id" placeholder="请输入用户名">
-      <br>密码：
-      <input type="password" v-model="User.password" placeholder="请输入密码">
-      <br>
-      <button v-on:click="login">登录</button>
-      <br>登录验证情况：
-      <textarea cols="30" rows="10" v-model="responseResult"></textarea>
+  <div class="box">
+    <img src="https://www.luffycity.com/static/img/Loginbg.3377d0c.jpg" alt>
+    <div class="login">
+      <div class="login-title">
+        <img src="https://www.luffycity.com/static/img/Logotitle.1ba5466.png" alt>
+        <p></p>
+      </div>
+      <div class="login_box">
+        <div class="title">
+          <span>登录</span>
+        </div>
+        <div class="inp">
+          <input v-model="User.id" type="text" placeholder="用户名 / 手机号码" class="user">
+          <input v-model="User.password" type="password" name class="pwd" placeholder="密码">
+          <div id="geetest"></div>
+          <div class="rember">
+            <p>
+              <input type="checkbox" class="no" name="a">
+              <span>记住密码</span>
+            </p>
+            <p>忘记密码</p>
+          </div>
+          <button class="login_btn" @click="loginHandler">登录</button>
+          <p class="go_login">
+            没有账号?
+            <span>立即注册</span>
+          </p>
+        </div>
+      </div>
     </div>
-    <hr>
-    <!-- <blog-footer></blog-footer> -->
   </div>
 </template>
 
 <script>
 import blogHeader from "@/components/common/BlogHeader.vue";
-// import blogFooter from "@/components/common/BlogFooter.vue";
 
 export default {
   name: "BlogLogin",
@@ -32,7 +46,7 @@ export default {
     };
   },
   methods: {
-    login() {
+    loginHandler() {
       this.$axios
         .post("/SSM/api/login", {
           id: this.User.id,
@@ -40,7 +54,6 @@ export default {
         })
         .then(successResponse => {
           console.log(successResponse.data);
-          this.responseResult = JSON.stringify(successResponse.data);
           if (successResponse.data.code == 200) {
             this.$router.replace({ path: "/index" });
           }
@@ -50,3 +63,148 @@ export default {
   }
 };
 </script>
+
+<style lang="css" scoped>
+.box {
+  width: 100%;
+  position: relative;
+}
+.box img {
+  width: 100%;
+}
+.box .login {
+  position: absolute;
+  width: 500px;
+  height: 400px;
+  top: 50%;
+  left: 50%;
+  margin-left: -250px;
+  margin-top: -300px;
+}
+.login .login-title {
+  width: 100%;
+  text-align: center;
+}
+.login-title img {
+  width: 190px;
+  height: auto;
+}
+.login-title p {
+  font-family: PingFangSC-Regular;
+  font-size: 18px;
+  color: #fff;
+  letter-spacing: 0.29px;
+  padding-top: 10px;
+  padding-bottom: 50px;
+}
+.login_box {
+  width: 400px;
+  height: auto;
+  background: #fff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
+  border-radius: 4px;
+  margin: 0 auto;
+  padding-bottom: 40px;
+}
+.login_box .title {
+  font-size: 20px;
+  color: #9b9b9b;
+  letter-spacing: 0.32px;
+  border-bottom: 1px solid #e6e6e6;
+  display: flex;
+  justify-content: space-around;
+  padding: 50px 60px 0 60px;
+  margin-bottom: 20px;
+  cursor: pointer;
+}
+.login_box .title span:nth-of-type(1) {
+  color: #4a4a4a;
+  border-bottom: 2px solid #84cc39;
+}
+
+.inp {
+  width: 350px;
+  margin: 0 auto;
+}
+.inp input {
+  border: 0;
+  outline: 0;
+  width: 100%;
+  height: 45px;
+  border-radius: 4px;
+  border: 1px solid #d9d9d9;
+  text-indent: 20px;
+  font-size: 14px;
+  background: #fff !important;
+}
+.inp input.user {
+  margin-bottom: 16px;
+}
+.inp .rember {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  margin-top: 10px;
+}
+.inp .rember p:first-of-type {
+  font-size: 12px;
+  color: #4a4a4a;
+  letter-spacing: 0.19px;
+  margin-left: 22px;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  /*position: relative;*/
+}
+.inp .rember p:nth-of-type(2) {
+  font-size: 14px;
+  color: #9b9b9b;
+  letter-spacing: 0.19px;
+  cursor: pointer;
+}
+
+.inp .rember input {
+  outline: 0;
+  width: 15px;
+  height: 45px;
+  border-radius: 4px;
+  border: 1px solid #d9d9d9;
+  text-indent: 20px;
+  font-size: 14px;
+  background: #fff !important;
+}
+
+.inp .rember p span {
+  display: inline-block;
+  font-size: 12px;
+  width: 100px;
+  /*position: absolute;*/
+  /*left: 20px;*/
+}
+#geetest {
+  margin-top: 20px;
+}
+.login_btn {
+  width: 100%;
+  height: 45px;
+  background: #84cc39;
+  border-radius: 5px;
+  font-size: 16px;
+  color: #fff;
+  letter-spacing: 0.26px;
+  margin-top: 30px;
+}
+.inp .go_login {
+  text-align: center;
+  font-size: 14px;
+  color: #9b9b9b;
+  letter-spacing: 0.26px;
+  padding-top: 20px;
+}
+.inp .go_login span {
+  color: #84cc39;
+  cursor: pointer;
+}
+</style>
